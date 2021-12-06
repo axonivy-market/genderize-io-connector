86d1a3877a75c86a128c8e01a110fbfac85420c6
Louis Müller <louis.mueller@axonivy.com>
2021-12-06 14:26:57 +0100

    updated annotation

--- genderize-io-connector-test/src_test/com/axonivy/connector/genderize/test/GenderizeIoDemoTest.java
+++ genderize-io-connector-test/src_test/com/axonivy/connector/genderize/test/GenderizeIoDemoTest.java
@@ -8,8 +8,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
+import com.axonivy.ivy.webtest.IvyWebTest;
 import com.axonivy.ivy.webtest.engine.EngineUrl;
 
+@IvyWebTest
 public class GenderizeIoDemoTest {
 
   private void genderRequest(String firstname, String expectedSaluation) {
